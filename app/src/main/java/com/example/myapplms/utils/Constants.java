2344258a87f044bf65dffd99b79b8b5e220669bf
Giang <giang1111100@gmail.com>
2026-05-09 21:04:26 +0700

    sua utils/constants

--- app/src/main/java/com/example/myapplms/utils/Constants.java
+++ app/src/main/java/com/example/myapplms/utils/Constants.java
@@ -1,7 +1,7 @@
-package com.example.myapplms.utils;
-
-public class Constants {
-    public static final String BASE_URL = "http://10.0.2.2:8080/api/";
-    //192.168.0.105
-    //192.168.1.86
-}
+//package com.example.myapplms.utils;
+//
+//public class Constants {
+//    public static final String BASE_URL = "http://192.168.1.9:8080/api/";
+//    //192.168.0.105
+//    //192.168.1.86
+//}
